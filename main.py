5e69ef7b65fb3628261b24aaaffb996cd73c06ad
AndreiVashenko <148420897+AndreiVashenko@users.noreply.github.com>
2023-12-05 09:41:43 +0000

    v3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import socket
 
-_command = b'https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getme'
+_command = b'http://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates'
 HOST = 'api.telegram.org'  # The remote host
 PORT = 80  # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, PORT))
   s.sendall(_command)
-  data = s.recv(443)
-print('Received', repr(data))
+  data = s.recv(1024)
+  if data:
+    print('Received', repr(data))
