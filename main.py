cd3f900cdbcc12de700f10a1bff97513c623dd46
AndreiVashenko <148420897+AndreiVashenko@users.noreply.github.com>
2023-12-11 10:24:36 +0000

    v4

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import socket
+import requests
 
-_command = b'http://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates'
+
+r = requests.post('https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?chat_id=6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg&text=Privet')
+
+statusCod = r.status_code;
+
+
+#_command = b'http://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates'
+
+#_command = 'GET /bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates #HTTP/1.1\r\nHost:api.telegram.org\r\n\r\n'.encode()
+
+_command = 'GET  https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates HTTP/1.1\r\n\r\n'.encode()
+
+#_command = 'GET /api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?chat_id=6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg&text=Privet HTTP/1.1\r\nHost:api.telegram.org\r\n\r\n'.encode()
+
 HOST = 'api.telegram.org'  # The remote host
-PORT = 80  # The same port as used by the server
+PORT = 443 # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, PORT))
   s.sendall(_command)
   data = s.recv(1024)
   if data:
-    print('Received', repr(data))
+    #pass
+    print('Received:\n\r', repr(data))
+
+
