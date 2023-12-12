5d4ebc4dbc0dd3aa1c30bbc02e1098913703c305
AndreiVashenko <148420897+AndreiVashenko@users.noreply.github.com>
2023-12-12 09:01:37 +0000

    123

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import socket
 import requests
 
+#mess = requests.get('https://api.telegram.org:443/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?#&chat_id=6044383569&text=Privet')
 
-mess = requests.get('https://api.telegram.org:443/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?&chat_id=6044383569&text=Privet')
-if not mess:
-  print('error')
+#mess = requests.get('https://api.telegram.org:443/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates')
 
+#if not mess:
+#  print('error')
 
 #statusCod = r.status_code;
 
+#_command = b'GET #https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?#&chat_id=6044383569&text=Privet HTTP/1.1'
 
-#_command = b'http://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates'
+_command = b'GET https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates'
 
-#_command = 'GET /bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates #HTTP/1.1\r\nHost:api.telegram.org\r\n\r\n'.encode()
 
-#_command = 'GET https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/getUpdates'.encode()
-_command = 'GET https://api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?chat_id=6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg&text=Privet'.encode()
-
-#_command = 'GET /api.telegram.org/bot6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg/sendMessage?chat_id=6414293222:AAHaU0kLcmya5GIqyhWZYcdiYCLf3O0c_rg&text=Privet HTTP/1.1\r\nHost:api.telegram.org\r\n\r\n'.encode()
 
 HOST = 'api.telegram.org'  # The remote host
-PORT = 443 # The same port as used by the server
+PORT = 443  # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((HOST, PORT))
-  s.send(_command)
+  s.sendall(_command)
   data = s.recv(1024)
   if data:
     #pass
     print('Received:\n\r', repr(data))
-
-
